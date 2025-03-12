de5a3df7b9703314ea34aede6795f90dbc40fabc
Alexander King Perocho <aperocho@uwo.ca>
2025-03-12 18:55:11 -0400

    refactor

--- include/chess.h
+++ include/chess.h
@@ -6,6 +6,7 @@
 #define CHESS_H
 
 #include <string>
+
 namespace Chess
 {
     enum Piece
@@ -23,17 +24,20 @@ namespace Chess
         q,
         k
     };
+
     enum Color
     {
         WHITE,
         BLACK,
         BOTH
     };
+
     enum SlidingPiece
     {
         ROOK,
         BISHOP
     };
+
     enum Square
     {
         a8,
@@ -102,16 +106,18 @@ namespace Chess
         h1,
         NO_SQUARE
     };
-    enum CastlePrivelage
+
+    enum CastlePrivilege
     {
         NO_CASTLE = 0, // 0000 0000 0000 0000 0000 0000 0000 0000
-        wk = 1,        // 0000 0000 0000 0000 0000 0000 0000 0001
-        wq = 2,        // 0000 0000 0000 0000 0000 0000 0000 0010
-        bk = 4,        // 0000 0000 0000 0000 0000 0000 0000 0100
-        bq = 8         // 0000 0000 0000 0000 0000 0000 0000 1000
+        wk = 1, // 0000 0000 0000 0000 0000 0000 0000 0001
+        wq = 2, // 0000 0000 0000 0000 0000 0000 0000 0010
+        bk = 4, // 0000 0000 0000 0000 0000 0000 0000 0100
+        bq = 8 // 0000 0000 0000 0000 0000 0000 0000 1000
     };
-    const char ASCII_PIECES[12] = {'P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k'};
-    const int ASCII_INT[256] = {
+
+    constexpr char ASCII_PIECES[12] = {'P', 'N', 'B', 'R', 'Q', 'K', 'p', 'n', 'b', 'r', 'q', 'k'};
+    constexpr int ASCII_INT[256] = {
         ['P'] = P,
         ['N'] = N,
         ['B'] = B,
@@ -123,8 +129,9 @@ namespace Chess
         ['b'] = b,
         ['r'] = r,
         ['q'] = q,
-        ['k'] = k};
-    const char PROMOTED_PIECE[] = {
+        ['k'] = k
+    };
+    constexpr char PROMOTED_PIECE[] = {
         [Q] = 'q',
         [R] = 'r',
         [B] = 'b',
@@ -132,7 +139,8 @@ namespace Chess
         [q] = 'q',
         [r] = 'r',
         [b] = 'b',
-        [n] = 'n'};
+        [n] = 'n'
+    };
     const std::string SQUARE_TO_ALGEBRAIC_NOTATION[64] = {
         "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8",
         "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7",
@@ -141,7 +149,8 @@ namespace Chess
         "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4",
         "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3",
         "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
-        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"};
+        "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"
+    };
 }
 
 #endif //CHESS_H
--- src/chessboard.cpp
+++ src/chessboard.cpp
@@ -9,12 +9,13 @@
 Chessboard::Chessboard()
 {
     enpassant_square = Chess::NO_SQUARE;
-    castle_privelage = Chess::NO_CASTLE;
+    castle_privilege = Chess::NO_CASTLE;
     side_to_move = 0;
 }
-Chessboard Chessboard::Copy() { return *this; };
-Bitboard Chessboard::GetPieceOccupancy(int piece) const { return piece_occupancies[piece]; }
-Bitboard Chessboard::GetColorOccupancy(int color) const { return color_occupancies[color]; }
+
+Chessboard Chessboard::Copy() const { return *this; }
+Bitboard Chessboard::GetPieceOccupancy(const int piece) const { return piece_occupancies[piece]; }
+Bitboard Chessboard::GetColorOccupancy(const int color) const { return color_occupancies[color]; }
 int Chessboard::GetSideToMove() const { return side_to_move; }
 int Chessboard::GetEnpassantSquare() const { return enpassant_square; }
-int Chessboard::GetCastlePrivelage() const { return castle_privelage; }
+int Chessboard::GetCastlePrivilege() const { return castle_privilege; }
