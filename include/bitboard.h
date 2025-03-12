a0ad8f08121b9624c627c7bf6c709744a9678b0f
Alexander King Perocho <aperocho@uwo.ca>
2025-03-12 18:54:29 -0400

    added operators and allowed implicit conversion

--- include/bitboard.h
+++ include/bitboard.h
@@ -11,86 +11,111 @@ class Bitboard
 {
 public:
     Bitboard();
-    explicit Bitboard(uint64_t ull);
+    Bitboard(uint64_t ull);
+
     class BitProxy
     {
     public:
-        BitProxy(Bitboard &bitboard, int square);
-        inline BitProxy &operator=(bool value)
+        BitProxy(Bitboard& bitboard, int square);
+
+        inline BitProxy& operator=(const bool value)
         {
             value ? bitboard.value |= (1ULL << square) : bitboard.value &= ~(1ULL << square);
             return *this;
-        };
-        inline explicit operator bool() const { return (bitboard.value & (1ULL << square)); } // != 0ULL
+        }
+
+        inline explicit operator bool() const { return bitboard.value != 0; } // != 0ULL
 
     private:
-        Bitboard &bitboard;
+        Bitboard& bitboard;
         int square;
     };
-    inline BitProxy operator[](const int square) { return BitProxy(*this, square); }
+
+    // Implicit Type Conversion
+    inline explicit operator bool() const { return value != 0; } // != 0ULL
+    inline operator int() const { return static_cast<int>(value); }
     inline explicit operator uint64_t() const { return value; }
-    inline bool operator[](int square) const { return (value & (1ULL << square)) != 0; } // or (bitboard & 1ULL << (square)); ?
 
-    inline Bitboard operator&(const uint64_t ull) const { return Bitboard(value & ull); }
-    inline Bitboard operator|(const uint64_t ull) const { return Bitboard(value | ull); }
-    inline Bitboard operator^(const uint64_t ull) const { return Bitboard(value ^ ull); }
+    inline BitProxy operator[](const int square) { return {*this, square}; }
+    inline bool operator[](const int square) const { return (value & (1ULL << square)) != 0; }
+    // or (bitboard & 1ULL << (square)); ?
 
-    inline Bitboard operator&(const Bitboard other) const { return Bitboard(value & other.value); }
-    inline Bitboard operator|(const Bitboard other) const { return Bitboard(value | other.value); }
-    inline Bitboard operator^(const Bitboard other) const { return Bitboard(value ^ other.value); }
+    inline Bitboard operator&(const uint64_t ull) const { return {value & ull}; }
+    inline Bitboard operator|(const uint64_t ull) const { return {value | ull}; }
+    inline Bitboard operator^(const uint64_t ull) const { return {value ^ ull}; }
+    inline Bitboard operator*(const uint64_t ull) const { return {value * ull}; }
 
-    inline Bitboard operator~() const { return Bitboard(~value); }
-    inline Bitboard operator<<(int shift) const { return Bitboard(value << shift); }
-    inline Bitboard operator>>(int shift) const { return Bitboard(value >> shift); }
-    inline Bitboard &operator&=(const Bitboard other)
+    inline Bitboard operator&(const Bitboard other) const { return {value & other.value}; }
+    inline Bitboard operator|(const Bitboard other) const { return {value | other.value}; }
+    inline Bitboard operator^(const Bitboard other) const { return {value ^ other.value}; }
+    inline Bitboard operator*(const Bitboard other) const { return {value * other.value}; }
+
+
+    inline Bitboard operator~() const { return {~value}; }
+    inline Bitboard operator<<(const int shift) const { return {value << shift}; }
+    inline Bitboard operator>>(const int shift) const { return {value >> shift}; }
+    inline Bitboard operator*(const int i) const { return {value * i}; }
+
+    inline Bitboard& operator&=(const Bitboard other)
     {
         value &= other.value;
         return *this;
     }
-    inline Bitboard &operator|=(const Bitboard other)
+
+    inline Bitboard& operator|=(const Bitboard other)
     {
         value |= other.value;
         return *this;
     }
-    inline Bitboard &operator^=(const Bitboard other)
+
+    inline Bitboard& operator^=(const Bitboard other)
     {
         value ^= other.value;
         return *this;
     }
-    inline Bitboard &operator<<=(int shift)
+
+    inline Bitboard& operator<<=(const int shift)
     {
         value <<= shift;
         return *this;
     }
-    inline Bitboard &operator>>=(int shift)
+
+    inline Bitboard& operator>>=(const int shift)
     {
         value >>= shift;
         return *this;
     }
-    inline Bitboard &operator*=(const Bitboard other)
+
+    inline Bitboard& operator*=(const Bitboard other)
     {
         value *= other.value;
         return *this;
     }
-    inline bool Test(int square) const { return operator[](square); }
-    inline Bitboard &Set(int square)
+
+    [[nodiscard]] inline bool Test(const int square) const { return operator[](square); }
+
+    inline Bitboard& Set(const int square)
     {
         value |= (1ULL << square);
         return *this;
     }
-    inline Bitboard &Clear(int square)
+
+    inline Bitboard& Clear(const int square)
     {
         value &= ~(1ULL << square);
         return *this;
     }
-    inline int GetLsbIndex() const { return value ? __builtin_ctzll(value) : -1; }
-    inline int CountBits() const { return __builtin_popcountll(value); }
+
+    [[nodiscard]] inline int GetLsbIndex() const { return value ? __builtin_ctzll(value) : -1; }
+    [[nodiscard]] inline int CountBits() const { return __builtin_popcountll(value); }
+
     inline int PopLsbIndex()
     {
-        int index = GetLsbIndex();
+        const int index = GetLsbIndex();
         value &= value - 1;
         return (index < 64) ? index : -1;
     }
+
     void Print() const;
 
 private:
