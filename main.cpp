bb26a730c3c4ffb47e831c88e31dca106713339c
Alexander King Perocho <aperocho@uwo.ca>
2025-03-12 18:56:48 -0400

    test attack_mask build

--- main.cpp
+++ main.cpp
@@ -3,9 +3,11 @@
 //
 
 #include "include/chessboard.h"
+#include "include/attack_mask.h"
 
 int main()
 {
+  AttackMask::initialize();
   const Chessboard board;
   board.GetColorOccupancy(2).Print();
   return 0;
